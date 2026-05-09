bb5bce66c287012f016a087a3d7ae1876d7f7897
namdongyeob <ehdduq1324@gmail.com>
2026-05-10 02:50:15 +0900

    refactor: Lv2-3 Validation 어노테이션으로 비밀번호 검증 이동

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.user.dto.request.UserChangePasswordRequest;
@@ -13,15 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    @GetMapping("/users/{userId}")
-    public ResponseEntity<UserResponse> getUser(@PathVariable long userId) {
-        return ResponseEntity.ok(userService.getUser(userId));
-    }
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<UserResponse> getUser(@PathVariable long userId) {
+		return ResponseEntity.ok(userService.getUser(userId));
+	}
 
-    @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser, @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
-        userService.changePassword(authUser.getId(), userChangePasswordRequest);
-    }
+	@PutMapping("/users")
+	public void changePassword(@Auth AuthUser authUser,
+		@Valid @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
+		userService.changePassword(authUser.getId(), userChangePasswordRequest);
+	}
 }
--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
-    private String oldPassword;
-    @NotBlank
-    private String newPassword;
+	@NotBlank(message = "기존 비밀번호를 입력해주세요.")
+	private String oldPassword;
+	@NotBlank(message = "새 비밀번호를 입력해주세요.")
+	@Pattern(
+		regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+		message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+	private String newPassword;
 }
